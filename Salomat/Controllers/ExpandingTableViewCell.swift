a220c6c3b3af3bf42d199b1f0ff566008815ff62
calfsama <tomirisnegmatova@gmail.com>
2022-12-21 16:08:58 +0500

    cart

---
+++ Salomat/Controllers/ExpandingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ExpandingTableViewCell.swift
+//  Salomat
+//
+//  Created by Tomiris Negmatova on 20/12/22.
+//
+
+import UIKit
+
+class ExpandingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Salomat/Models/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  Salomat
+//
+//  Created by Tomiris Negmatova on 19/12/22.
+//
+
+import Foundation
